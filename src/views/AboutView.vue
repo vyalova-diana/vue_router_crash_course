3d25a6afa55b9382a138a1a0fa153b8c812c6742
Diana <vyalova.diana@yandex.ru>
2023-02-15 18:36:57 +0300

    Initial commit basic router example with anchor links

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="container">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad beatae commodi eligendi eos esse eveniet facilis iusto numquam officia omnis, porro qui rem similique soluta tenetur! Facere iste tenetur vel. </p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AboutVie"
+};
+</script>
+
+<style scoped>
+p {
+  text-align: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import carsData from "../data.json"
+import {ref} from "vue";
+
+const cars = ref(carsData);
+</script>
+
+<template>
+  <main class="container">
+
+    <h1>Our Cars</h1>
+    <div class="cards">
+      <div v-for="car in cars" :key="car.id" class="card">
+        <h1>{{car.make}}</h1>
+        <p>${{car.price}}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.cards {
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 15px 20px;
+}
+.card {
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.207);
+  padding: 15px;
+  width: 150px;
+  cursor: pointer;
+}
+
+</style>
